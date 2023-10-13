565a43d7d4203aa80419cf9a6b59d3a8842129d9
Aadil Gani <aadilgani2001@gmail.com>
2023-10-13 13:30:13 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import CreateArea from "./components/CreateArea";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Note from "./components/Note";
-
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
